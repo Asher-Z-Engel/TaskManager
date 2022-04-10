66d489d094c5e627de302594cae8b359f7fa52b6
Asher <asherzengel@gmail.com>
2022-04-10 21:19:55 +0300

    error handling

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const app = express()
 const tasks = require('./routes/tasks')
 const { connectDB } = require('./db/connect')
 require('dotenv').config()
+// Not found
+const notFound = require('./utils/notFound')
+// error handler
+const errorHandler = require('./errors/errorHandler')
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use('/api/v1/tasks', tasks)
 
-const port = 5000
+app.use(notFound)
+app.use(errorHandler)
+
+const port = process.env.PORT || 5000
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
-const wrapAsync = require('../async/wrapAsync')
+const wrapAsync = require('../utils/async')
+const CustomError = require('../errors/CustomError')
 
 const getAllTasks = wrapAsync(async (req, res) => {
     const tasks = await Task.find({})
@@ -11,16 +12,16 @@ const createTask = wrapAsync(async (req, res) => {
     res.status(201).json({ task })
 })
 
-const getTask = wrapAsync(async (req, res) => {
+const getTask = wrapAsync(async (req, res, next) => {
     const { id: taskID } = req.params;
     const task = await Task.findOne({_id: taskID})
-    if (!task) {
-      return res.status(404).json({ msg: `Cannot find task with id: ${taskID}` })
+  if (!task) {
+      return next(new CustomError(404, `Cannot find task with id: ${taskID}`))
     }
     res.status(200).json({task})
 })
 
-const updateTask = wrapAsync(async (req, res) => {
+const updateTask = wrapAsync(async (req, res, next) => {
     const { id: taskID } = req.params
     const data = req.body
     const task = await Task.findOneAndUpdate({ _id: taskID }, data, {
@@ -28,16 +29,16 @@ const updateTask = wrapAsync(async (req, res) => {
       runValidators: true
     })
     if (!task) {
-      return res.status(404).json({ msg: `Cannot find task with id: ${taskID}` })
+      return next(new CustomError(404, `Cannot find task with id: ${taskID}`))
     }
     res.status(200).json({task})  
 })
 
-const deleteTask = wrapAsync(async (req, res) => {
+const deleteTask = wrapAsync(async (req, res, next) => {
     const { id: taskID } = req.params
     const task = await Task.findOneAndDelete({ _id: taskID })
     if (!task) {
-      return res.status(404).json({ msg: `Cannot find task with id: ${taskID}` })
+      return next(new CustomError(404, `Cannot find task with id: ${taskID}`))
     }
     res.status(200).json({task})
 })
